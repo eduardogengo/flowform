05bcb989617834d3ea2786624be7fe63d952580d
Eduardo Gengo <eduardogengo@yahoo.com.br>
2025-12-19 11:22:20 -0300

    validation of each step activated

--- src/app/features/form/pages/wizard/wizard.ts
+++ src/app/features/form/pages/wizard/wizard.ts
@@ -1,15 +1,33 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  FormsModule,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { ButtonModule } from 'primeng/button';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
 import { StepperModule } from 'primeng/stepper';
 
 @Component({
   selector: 'app-wizard',
-  imports: [StepperModule, ButtonModule, ReactiveFormsModule, JsonPipe],
+  imports: [
+    StepperModule,
+    ButtonModule,
+    FormsModule,
+    InputTextModule,
+    IftaLabelModule,
+    ReactiveFormsModule,
+    JsonPipe,
+    MessageModule,
+  ],
   templateUrl: './wizard.html',
   styleUrl: './wizard.css',
 })
@@ -18,15 +36,67 @@ export class Wizard {
 
   constructor(private fb: FormBuilder, private router: Router) {
     this.form = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', Validators.required],
     });
   }
 
+  isFormValid(step?: number): boolean {
+    if (!step) {
+      return this.form.valid;
+    } else {
+      const isStepValid = this.isStepValid(step);
+      console.log('isStepValid', isStepValid);
+      return isStepValid;
+    }
+  }
+
+  private isStepValid(step: number): boolean {
+    console.log('step', step);
+    switch (step) {
+      case 1:
+        return !!(this.form.get('firstName')?.valid && this.form.get('lastName')?.valid);
+      case 2:
+        return !!(this.form.get('email')?.valid && this.form.get('phone')?.valid);
+      default:
+        return false;
+    }
+  }
+
+  isFieldInvalid(controlName: string) {
+    const control = this.form.get(controlName);
+    return control?.invalid && control.touched;
+  }
+
   submit() {
     console.log(this.form.value);
     this.router.navigate(['/success']);
+  }
+
+  onNext(activateCallback: (step: number) => void, from: number, to: number) {
+    if (this.isFormValid(from)) {
+      activateCallback(to);
+    } else {
+      this.markStepTouched(from);
+    }
+  }
+
+  onBack(activateCallback: (step: number) => void, to: number) {
+    activateCallback(to);
+  }
+
+  private markStepTouched(step: number) {
+    switch (step) {
+      case 1:
+        this.form.get('firstName')?.markAsTouched();
+        this.form.get('lastName')?.markAsTouched();
+        break;
+      case 2:
+        this.form.get('email')?.markAsTouched();
+        this.form.get('phone')?.markAsTouched();
+        break;
+    }
   }
 }
