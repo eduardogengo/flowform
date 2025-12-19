d32cb3fb6a26358e0d636489896d6f5a6da19c3f
Eduardo Gengo <eduardogengo@yahoo.com.br>
2025-12-19 09:22:42 -0300

    simple form created and after confirmation connected to success page

--- src/app/features/form/pages/wizard/wizard.ts
+++ src/app/features/form/pages/wizard/wizard.ts
@@ -1,11 +1,32 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ButtonModule } from 'primeng/button';
+import { StepperModule } from 'primeng/stepper';
 
 @Component({
   selector: 'app-wizard',
-  imports: [],
+  imports: [StepperModule, ButtonModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './wizard.html',
   styleUrl: './wizard.css',
 })
 export class Wizard {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+    });
+  }
+
+  submit() {
+    console.log(this.form.value);
+    this.router.navigate(['/success']);
+  }
 }
