9fdd20b60a786d59f6bb55f3278a0eaf1f09758b
Eduardo Gengo <eduardogengo@yahoo.com.br>
2026-01-12 11:21:21 -0300

    footer component created

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { Header } from './core/components/header/header';
+import { Footer } from "./core/components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, Header],
+  imports: [RouterOutlet, ButtonModule, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
