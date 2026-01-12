823f09ef88bbe29f7b6335a97702b9832518fc7f
Eduardo Gengo <eduardogengo@yahoo.com.br>
2026-01-12 11:13:02 -0300

    header component created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { Header } from './core/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
